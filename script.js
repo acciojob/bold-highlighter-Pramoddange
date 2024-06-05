8bdc36a7134b0bea66ba37bdae7bb1a9d193f813
Pramod Dange <123957464+Pramoddange@users.noreply.github.com>
2024-06-05 12:56:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function highlight() {
     //Write your code here
-
+  let strong=document.querySelectorAll('strong')
+	 strong.forEach(element => {
+        element.style.color = 'green';
+    });
 
 }
 
 
 function return_normal() {
     //Write your code here
+let strong=document.querySelectorAll('strong')
+     strong.forEach(element => {
+        element.style.color = 'black';
+    });
 
-    
 }
